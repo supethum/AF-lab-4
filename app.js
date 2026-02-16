30ea296950dc528f324e11ea5ac85e5cc53cdfa7
ravindu-sliit <it23829510@my.sliit.lk>
2026-02-16 13:06:34 +0530

    recheck

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ if (err) throw err;
 console.log(data); 
 }); 
 
-const fs = require('fs'); 
 fs.writeFile('file.txt', 'Hello World!', function (err) { 
   if (err) throw err; 
   console.log('File saved!'); 
