4a9568d362b50888a77e1598c60d3e977abd5c39
supethumpathirana <185578501+supethum@users.noreply.github.com>
2026-02-16 12:32:29 +0530

    Promises

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-const myModule = require('./my-module.js'); 
-console.log(myModule.myFunction());
+const condition = true; 
+
+const myPromise = new Promise((resolve, reject) => { 
+  if (condition) { 
+    resolve('Success!'); 
+  } else { 
+    reject('Failure!'); 
+  } 
+}); 
+
+myPromise
+  .then((result) => console.log("Promise Resolved:", result))
+  .catch((error) => console.log("Promise Rejected:", error));
